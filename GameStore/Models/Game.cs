47b3ee3b96aec0971293bd72f3db3b7dc7979dc5
Re1ncarnat10 <128154276+Re1ncarnat10@users.noreply.github.com>
2024-01-18 01:54:41 +0100

    game and genre

---
+++ GameStore/Models/Game.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace GameStore.Models
+{
+    [Table("Game")]
+    public class Game
+    {
+        
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+}
---
+++ GameStore/Models/Genre.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GameStore.Models
+{
+    [Table("Genre")]
+    public class Genre
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
