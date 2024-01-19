de258b22ab55840961150d01b42cca5491d5e26d
Re1ncarnat10 <128154276+Re1ncarnat10@users.noreply.github.com>
2024-01-19 21:22:05 +0100

    Wszystko dziala, dodawanie tez

---
+++ GameStore/ViewModels/CreateGameViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace GameStore.ViewModels
+{
+    public class CreateGameViewModel
+    {
+        public string GameName { get; set; }
+        public string Publisher { get; set; }
+        public string Developer { get; set; }
+        public string ReleaseDate { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public int GenreId { get; set; }
+    }
+}
